440e21b32ae6cbb6238ed581de001619ecea8f33
Ozelitotiktak <odioalpixel@gmail.com>
2016-12-01 10:38:10 +0100

    Comentado DataAccess

--- CV2016UltimateVersion/service/DataAccess.vb
+++ CV2016UltimateVersion/service/DataAccess.vb
@@ -7,7 +7,7 @@ Public Class DataAccess
         Try
             connection.Open()
         Catch ex As Exception
-            MsgBox("Error al conectar con la BD")
+            'MsgBox("Error al conectar con la BD")
         End Try
         Return connection
     End Function
@@ -32,7 +32,7 @@ Public Class DataAccess
         Try
             num = comn.ExecuteNonQuery()
         Catch ex As Exception
-            MsgBox("Error al ejecutar la consulta 1")
+            'MsgBox("Error al ejecutar la consulta 1")
         End Try
 
         disconnect(conex)
@@ -50,7 +50,7 @@ Public Class DataAccess
         Try
             adap.Fill(ds, "Users")
         Catch ex As Exception
-            MsgBox("Error al ejecutar la consulta 2")
+            'MsgBox("Error al ejecutar la consulta 2")
         End Try
 
         disconnect(conex)
