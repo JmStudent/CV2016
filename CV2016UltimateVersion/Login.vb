4337cfbd9bd1a8ea2a80f0280d4df9a84b905684
Ozelitotiktak <odioalpixel@gmail.com>
2016-12-01 14:28:05 +0100

    a saber...

--- CV2016UltimateVersion/Login.vb
+++ CV2016UltimateVersion/Login.vb
@@ -1,7 +1,4 @@
 ﻿Partial Class Login
-    'Inherits BaseForm
-    'Private isMouseDown As Boolean = False
-    'Private mouseOffset As Point
     Private bv As New Behavior
     Private Const userTxtDefault As String = "DNI Usuario"
     Private Const pwTxtDefault As String = "Password"
@@ -11,37 +8,7 @@
         tbxUser.Text = userTxtDefault
         tbxPW.Text = pwTxtDefault
     End Sub
-    ' FUNCTIONS FOR WINDOW MOVEMENT ------------------------------------------------------------------------
-
-    ' Left mouse button pressed
-    'Private Sub Login_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
-    '    If e.Button = sender.MouseButtons.Left Then
-    '        ' Get the new position
-    '        mouseOffset = New Point(-e.X, -e.Y)
-    '        ' Set that left button is pressed
-    '        isMouseDown = True
-    '    End If
-    'End Sub
-
-    '' MouseMove used to check if mouse cursor is moving
-    'Private Sub Login_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
-    '    If isMouseDown Then
-    '        Dim mousePos As Point = Control.MousePosition
-    '        ' Get the new form position
-    '        mousePos.Offset(mouseOffset.X, mouseOffset.Y)
-    '        Me.Location = mousePos
-    '    End If
-    'End Sub
-
-    '' Left mouse button released, form should stop moving
-    'Private Sub Login_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
-    '    If e.Button = sender.MouseButtons.Left Then
-    '        isMouseDown = False
-    '    End If
-    'End Sub
-    ' ------------------------------------------------------------------------------------------------------
     ' FUNCTIONS FOR DISPLAYING TOOLTIPS --------------------------------------------------------------------
-
     Private Sub tbxUser_Enter(ByVal sender As Object, ByVal e As EventArgs) Handles tbxUser.Enter
         bv.changeColor(tbxUser, userTxtDefault)
     End Sub
