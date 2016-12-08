4c58ea1479aba43bf374da55b27b3c28750e60df
GuilleLuna <guille.lunacerezo@gmail.com>
2016-12-08 14:10:13 +0100

    Add files via upload

---
+++ CV2016UltimateVersion/AdminPanel.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AdminPanel
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lvNoActive = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.btnActivate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.lbNoactive = New System.Windows.Forms.Label()
+        Me.btnGenerateCV = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lvActive = New System.Windows.Forms.ListView()
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader7 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.tbsearch = New System.Windows.Forms.TextBox()
+        Me.btnsearch = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lvNoActive
+        '
+        Me.lvNoActive.BackColor = System.Drawing.Color.White
+        Me.lvNoActive.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lvNoActive.CheckBoxes = True
+        Me.lvNoActive.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.lvNoActive.GridLines = True
+        Me.lvNoActive.Location = New System.Drawing.Point(30, 125)
+        Me.lvNoActive.Name = "lvNoActive"
+        Me.lvNoActive.Scrollable = False
+        Me.lvNoActive.Size = New System.Drawing.Size(280, 127)
+        Me.lvNoActive.TabIndex = 0
+        Me.lvNoActive.UseCompatibleStateImageBehavior = False
+        Me.lvNoActive.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "DNI"
+        Me.ColumnHeader1.Width = 90
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Email"
+        Me.ColumnHeader2.Width = 190
+        '
+        'btnActivate
+        '
+        Me.btnActivate.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(194, Byte), Integer))
+        Me.btnActivate.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnActivate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnActivate.ForeColor = System.Drawing.Color.Snow
+        Me.btnActivate.Location = New System.Drawing.Point(118, 298)
+        Me.btnActivate.Name = "btnActivate"
+        Me.btnActivate.Size = New System.Drawing.Size(97, 25)
+        Me.btnActivate.TabIndex = 2
+        Me.btnActivate.Text = "Activar"
+        Me.btnActivate.UseVisualStyleBackColor = False
+        '
+        'btnDelete
+        '
+        Me.btnDelete.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(194, Byte), Integer))
+        Me.btnDelete.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnDelete.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnDelete.Location = New System.Drawing.Point(320, 298)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(97, 25)
+        Me.btnDelete.TabIndex = 3
+        Me.btnDelete.Text = "Borrar"
+        Me.btnDelete.UseVisualStyleBackColor = False
+        '
+        'lbNoactive
+        '
+        Me.lbNoactive.AutoSize = True
+        Me.lbNoactive.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbNoactive.Location = New System.Drawing.Point(78, 92)
+        Me.lbNoactive.Name = "lbNoactive"
+        Me.lbNoactive.Size = New System.Drawing.Size(149, 20)
+        Me.lbNoactive.TabIndex = 4
+        Me.lbNoactive.Text = "Usuarios no Activos"
+        '
+        'btnGenerateCV
+        '
+        Me.btnGenerateCV.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(194, Byte), Integer))
+        Me.btnGenerateCV.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnGenerateCV.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnGenerateCV.ForeColor = System.Drawing.Color.Snow
+        Me.btnGenerateCV.Location = New System.Drawing.Point(522, 298)
+        Me.btnGenerateCV.Name = "btnGenerateCV"
+        Me.btnGenerateCV.Size = New System.Drawing.Size(97, 25)
+        Me.btnGenerateCV.TabIndex = 7
+        Me.btnGenerateCV.Text = "Generar CV"
+        Me.btnGenerateCV.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(481, 92)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(131, 20)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Usuarios  Activos"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(256, 33)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(214, 24)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Panel de Administración"
+        '
+        'lvActive
+        '
+        Me.lvActive.BackColor = System.Drawing.Color.White
+        Me.lvActive.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lvActive.CheckBoxes = True
+        Me.lvActive.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader3, Me.ColumnHeader7})
+        Me.lvActive.GridLines = True
+        Me.lvActive.Location = New System.Drawing.Point(425, 125)
+        Me.lvActive.Name = "lvActive"
+        Me.lvActive.Scrollable = False
+        Me.lvActive.Size = New System.Drawing.Size(280, 127)
+        Me.lvActive.TabIndex = 12
+        Me.lvActive.UseCompatibleStateImageBehavior = False
+        Me.lvActive.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "DNI"
+        Me.ColumnHeader3.Width = 90
+        '
+        'ColumnHeader7
+        '
+        Me.ColumnHeader7.Text = "Email"
+        Me.ColumnHeader7.Width = 190
+        '
+        'tbsearch
+        '
+        Me.tbsearch.Location = New System.Drawing.Point(294, 349)
+        Me.tbsearch.Multiline = True
+        Me.tbsearch.Name = "tbsearch"
+        Me.tbsearch.Size = New System.Drawing.Size(109, 31)
+        Me.tbsearch.TabIndex = 13
+        '
+        'btnsearch
+        '
+        Me.btnsearch.BackgroundImage = Global.CV2016UltimateVersion.My.Resources.Resources.search1
+        Me.btnsearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnsearch.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(194, Byte), Integer))
+        Me.btnsearch.Location = New System.Drawing.Point(409, 349)
+        Me.btnsearch.Name = "btnsearch"
+        Me.btnsearch.Size = New System.Drawing.Size(41, 31)
+        Me.btnsearch.TabIndex = 14
+        Me.btnsearch.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(194, Byte), Integer))
+        Me.btnClose.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(194, Byte), Integer))
+        Me.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnClose.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClose.ForeColor = System.Drawing.Color.White
+        Me.btnClose.Location = New System.Drawing.Point(717, -1)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(20, 30)
+        Me.btnClose.TabIndex = 15
+        Me.btnClose.Text = "X"
+        Me.btnClose.UseVisualStyleBackColor = False
+        '
+        'AdminPanel
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.BackgroundImage = Global.CV2016UltimateVersion.My.Resources.Resources.admin2
+        Me.ClientSize = New System.Drawing.Size(737, 412)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnsearch)
+        Me.Controls.Add(Me.tbsearch)
+        Me.Controls.Add(Me.lvActive)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnGenerateCV)
+        Me.Controls.Add(Me.lbNoactive)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnActivate)
+        Me.Controls.Add(Me.lvNoActive)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "AdminPanel"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Panel de Administración"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lvNoActive As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents btnActivate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents lbNoactive As Label
+    Friend WithEvents btnGenerateCV As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lvActive As ListView
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ColumnHeader7 As ColumnHeader
+    Friend WithEvents tbsearch As TextBox
+    Friend WithEvents btnsearch As Button
+    Friend WithEvents btnClose As Button
+End Class
---
+++ CV2016UltimateVersion/AdminPanel.vb
@@ -0,0 +1,254 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class AdminPanel
+    Dim ad As New DataAccess
+
+    Private isMouseDown As Boolean = False
+    Private mouseOffset As Point
+
+
+    Private Sub AdminPanel_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tbsearch.Text = "Buscar por DNI"
+        loadpeople(0, lvNoActive)
+        loadpeople(2, lvActive)
+
+
+    End Sub
+
+    Public Sub loadpeople(tipo As Integer, lv As ListView) 'carga en los listview los usuarios, primer parametro tipo de user, segundo parametro; el listview donde se quiere cargar
+
+        Dim query As String
+        Dim slqadpter As New MySqlDataAdapter
+        Dim sqlcommand As New MySqlCommand
+        Dim TABLE As New DataTable
+        Dim i As Integer
+
+        lv.Items.Clear()
+
+
+        query = "SELECT dni, email FROM perfil where dni IN (select dni from usuarios where tipo=" & tipo & ")"
+
+        With sqlcommand
+            .CommandText = query
+            .Connection = ad.connect()
+        End With
+
+        With slqadpter
+            .SelectCommand = sqlcommand
+            .Fill(TABLE)
+        End With
+
+        For i = 0 To TABLE.Rows.Count - 1
+            With lv
+                .Items.Add(TABLE.Rows(i)("dni"))
+                With .Items(.Items.Count - 1).SubItems
+                    .Add(TABLE.Rows(i)("email"))
+                End With
+            End With
+        Next
+
+        For i = 0 To lv.Items.Count - 2 Step 2
+            lv.Items(i + 1).BackColor = Color.LightGray
+            lv.Items(i).BackColor = Color.LightCyan
+        Next
+    End Sub
+
+    Private Sub btnActivate_Click(sender As Object, e As EventArgs) Handles btnActivate.Click 'Activación de usuarios'
+        Dim checkedItems As ListView.CheckedListViewItemCollection =
+        lvNoActive.CheckedItems
+        Dim dni As String
+
+        Dim query As String
+
+        For Each item In checkedItems
+            dni = item.SubItems(0).text
+            query = " UPDATE usuarios set tipo=2 WHERE dni= '" & dni & "'"
+            ad.cud(query)
+        Next
+
+
+        loadpeople(2, lvActive)
+        loadpeople(0, lvNoActive)
+
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click 'borrados de usuarios'
+        Dim checkedItems As ListView.CheckedListViewItemCollection = lvNoActive.CheckedItems
+        Dim checkedItems2 As ListView.CheckedListViewItemCollection = lvActive.CheckedItems
+        Dim dni As String
+        Dim num1, num2 As Integer
+        Dim query As String
+
+        num1 = checkedItems.Count
+        num2 = checkedItems2.Count
+
+        If (num1 > 0 Or num2 > 0) Then
+            Dim res = MsgBox("Estas seguro de borrar a al/los usuario/s?", MsgBoxStyle.YesNo)
+            If res = 6 Then
+                For Each item In checkedItems
+                    dni = item.SubItems(0).text
+                    query = " DELETE FROM  usuarios WHERE dni= '" & dni & "'"
+                    ad.cud(query)
+                Next
+
+                For Each item In checkedItems2
+                    dni = item.SubItems(0).text
+                    query = " DELETE FROM  usuarios WHERE dni= '" & dni & "'"
+                    ad.cud(query)
+                Next
+
+                loadpeople(2, lvActive)
+                loadpeople(0, lvNoActive)
+            End If
+
+
+        End If
+    End Sub
+
+
+
+    ' Left mouse button pressed
+    Private Sub Login_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        If e.Button = sender.MouseButtons.Left Then
+            ' Get the new position
+            mouseOffset = New Point(-e.X, -e.Y)
+            ' Set that left button is pressed
+            isMouseDown = True
+        End If
+    End Sub
+
+    ' MouseMove used to check if mouse cursor is moving
+    Private Sub Login_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        If isMouseDown Then
+            Dim mousePos As Point = Control.MousePosition
+            ' Get the new form position
+            mousePos.Offset(mouseOffset.X, mouseOffset.Y)
+            Me.Location = mousePos
+        End If
+    End Sub
+
+    ' Left mouse button released, form should stop moving
+    Private Sub Login_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
+        If e.Button = sender.MouseButtons.Left Then
+            isMouseDown = False
+        End If
+    End Sub
+
+
+    Private Sub btnsearch_Click(sender As Object, e As EventArgs) Handles btnsearch.Click 'buscar usuarios por dni'
+
+        Dim query, query2 As String
+        Dim slqadpter As New MySqlDataAdapter
+        Dim sqlcommand, sqlcommand2 As New MySqlCommand
+        Dim TABLE As New DataTable
+        Dim i As Integer
+        Dim lv As ListView
+
+
+        Dim dni = tbsearch.Text
+        If (Len(dni) = 9) Then
+            query = "SELECT dni, email FROM perfil where dni = '" & dni & "'"
+            query2 = "SELECT tipo FROM usuarios where dni = '" & dni & "'"
+
+
+            With sqlcommand2
+                .CommandText = query2
+                .Connection = ad.connect()
+
+            End With
+            With slqadpter
+                .SelectCommand = sqlcommand2
+            End With
+            Dim value As Object = sqlcommand2.ExecuteScalar()
+
+            If (value = 2) Then
+                lv = lvActive
+            Else
+                lv = lvNoActive
+            End If
+
+
+            With sqlcommand
+                .CommandText = query
+                .Connection = ad.connect()
+
+            End With
+            With slqadpter
+                .SelectCommand = sqlcommand
+                .Fill(TABLE)
+            End With
+
+            lvActive.Items.Clear()
+            lvNoActive.Items.Clear()
+
+            For i = 0 To TABLE.Rows.Count - 1
+                With lv
+                    .Items.Add(TABLE.Rows(i)("dni"))
+                    With .Items(.Items.Count - 1).SubItems
+                        .Add(TABLE.Rows(i)("email"))
+                    End With
+                End With
+            Next
+
+            For i = 0 To lv.Items.Count - 2 Step 2
+                lv.Items(i + 1).BackColor = Color.LightGray
+                lv.Items(i).BackColor = Color.LightCyan
+            Next
+        Else
+            MessageBox.Show("DNI no correcto", "Buscar", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            loadpeople(2, lvActive)
+            loadpeople(0, lvNoActive)
+        End If
+
+    End Sub
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click 'cerrar ventana'
+        Dim res As Integer
+        res = MessageBox.Show("¿Está seguro que desea salir?", "Salida", MessageBoxButtons.YesNo, MessageBoxIcon.Information)
+
+        If res = 6 Then
+            Application.Exit()
+        End If
+    End Sub
+
+
+
+    Private Sub tbsearch_GotFocus(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbsearch.GotFocus
+        If tbsearch.Text = "Buscar por DNI" Then
+            tbsearch.Text = ""
+        End If
+    End Sub
+    Private Sub Ttbsearch_LostFocus(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tbsearch.LostFocus
+        If tbsearch.TextLength = Nothing Then
+            tbsearch.Text = "Buscar por DNI"
+        End If
+    End Sub
+
+    Private Sub btnGenerateCV_Click(sender As Object, e As EventArgs) Handles btnGenerateCV.Click 'generar pdf'
+        Dim checkedItems As ListView.CheckedListViewItemCollection = lvNoActive.CheckedItems
+        Dim checkedItems2 As ListView.CheckedListViewItemCollection = lvActive.CheckedItems
+        Dim dni As String
+        Dim num1, num2, num3 As Integer
+        num1 = checkedItems.Count
+        num2 = checkedItems2.Count
+        num3 = num1 + num2
+
+
+        If (num3 = 1) Then
+            For Each item In checkedItems
+                dni = item.SubItems(0).text
+                Dim g As New PDFGenerator(dni)
+                g.generate()
+
+            Next
+            For Each item In checkedItems2
+                dni = item.SubItems(0).text
+                Dim g As New PDFGenerator(dni)
+                g.generate()
+            Next
+        Else
+            MessageBox.Show("Solo se puede generar un CV a la vez", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
+    End Sub
+End Class
+
